575867dc333a4eef167022ec62dec284f3d677e6
siria <siria55lee@gmail.com>
2025-09-24 18:00:46 +0800

    update

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"IDarz+U7kWDMnwKpvBi3tmNEsiEvaa2a6jrn1JwK34A=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
